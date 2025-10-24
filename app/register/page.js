2ba4eaad8e5d0ad3cddde6ba3a593f3f35208da7
shakibchakladar <shakibchakladar@gmail.com>
2025-10-24 19:25:27 +0600

    login,register and payment page added

---
+++ app/register/page.js
@@ -0,0 +1,19 @@
+import RegistrationForm from '@/components/auth/RegistrationForm'
+import SocialLogins from '@/components/auth/SocialLogins'
+import React from 'react'
+
+function RegisterPage() {
+  return (
+    <>
+     <section className="h-screen grid place-items-center mt-9">
+        <div className="max-w-[450px] w-full mx-auto p-6 border border-gray-700/20 rounded-md">
+          <h4 className="font-bold text-2xl">Sign in</h4>
+          <RegistrationForm />
+          <SocialLogins />
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default RegisterPage
