225bb3c5a9ac34ebe5a3a5b98c53573ab61e0443
shakibchakladar <shakibchakladar@gmail.com>
2025-10-25 20:31:39 +0600

    create hotel schema for db

---
+++ models/hotel-model.js
@@ -0,0 +1,77 @@
+import mongoose, { Schema } from "mongoose";
+
+const hotelSchema = new Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+
+  address1: {
+    required: true,
+    type: String,
+  },
+
+  airportCode: {
+    required: true,
+    type: String,
+  },
+  city: {
+    required: false,
+    type: String,
+  },
+  countryCode: {
+    required: false,
+    type: String,
+  },
+  highRate: {
+    required: false,
+    type: Number,
+  },
+  lowRate: {
+    required: false,
+    type: Number,
+  },
+  propertyCategory: {
+    required: false,
+    type: Number,
+  },
+  stateProvinceCode: {
+    required: false,
+    type: String,
+  },
+  thumbNailUrl: {
+    required: false,
+    type: String,
+  },
+  gallery: {
+    required: false,
+    type: Array,
+  },
+  overview: {
+    required: false,
+    type: String,
+  },
+  amenities: {
+    required: false,
+    type: Array,
+  },
+  location: {
+    required: false,
+    type: String,
+  },
+  locationDescription: {
+    required: false,
+    type: Sring,
+  },
+  postalCode: {
+    required: false,
+    type: String,
+  },
+  shortDescription: {
+    required: false,
+    type: String,
+  },
+});
+
+export const hotelModel =
+  mongoose.models.hotels || mongoose.model("hotels", hotelSchema);
