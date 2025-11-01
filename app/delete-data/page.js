07c2fd23ea6666bf63dccc42359c67fc97ee4226
shakibchakladar <shakibchakladar@gmail.com>
2025-11-01 18:04:12 +0600

    delete user data file create

---
+++ app/delete-data/page.js
@@ -0,0 +1,17 @@
+export default function DeleteDataPage() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "40px" }}>
+      <h1>Data Deletion Instructions</h1>
+      <p>
+        If you would like to delete your account and all associated data,
+        please email us at{" "}
+        <strong>shakibchakladar@gmail.com</strong> with the subject line{" "}
+        
+      </p>
+      <p>
+        We will process your request and permanently delete all user information
+        from our servers within 7 business days.
+      </p>
+    </div>
+  );
+}
