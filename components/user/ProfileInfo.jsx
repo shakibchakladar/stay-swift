ab4d149c6ba6cdf1bf16196f370e435b05510f6d
shakibchakladar <shakibchakladar@gmail.com>
2025-11-05 23:24:30 +0600

    profile info page update

--- components/user/ProfileInfo.jsx
+++ components/user/ProfileInfo.jsx
@@ -1,13 +1,19 @@
-const ProfileInfo = () => {
+import { auth } from "@/auth";
+import Image from "next/image";
+
+const ProfileInfo =async () => {
+  const session= await auth();
+  const user=session?.user;
+  console.log(" user who?",user)
   return (
     <div className="flex flex-col items-center py-8 text-center">
       <div className="relative max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] bg-orange-600 grid place-items-center text-4xl text-white">
-        S
+        <Image src={user?.image} height={20} width={20} alt="image"/>
       </div>
 
       <div>
-        <h3 className="text-2xl font-semibold lg:text-[28px]">Sumit Saha</h3>
-        <p className="leading-[231%] lg:text-lg">sumitsaha@gmail.com</p>
+        <h3 className="text-2xl font-semibold lg:text-[28px]">{user?.name}</h3>
+        <p className="leading-[231%] lg:text-lg">{user?.email}</p>
       </div>
 
       <div className="w-3/4 border-b border-[#a4a4a4] py-6 lg:py-4"></div>
