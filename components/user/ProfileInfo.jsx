0e033fbc0abbd04be755b2d9b612e68e84030794
shakibchakladar <shakibchakladar@gmail.com>
2025-11-08 11:33:25 +0600

    profile info updated

--- components/user/ProfileInfo.jsx
+++ components/user/ProfileInfo.jsx
@@ -1,14 +1,18 @@
 import { auth } from "@/auth";
 import Image from "next/image";
 
-const ProfileInfo =async () => {
-  const session= await auth();
-  const user=session?.user;
-  console.log(" user who?",user)
+const ProfileInfo = async () => {
+  const session = await auth();
+  const user = session?.user;
+  console.log(" user who?", user);
   return (
     <div className="flex flex-col items-center py-8 text-center">
-      <div className="relative max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] bg-orange-600 grid place-items-center text-4xl text-white">
-        <Image src={user?.image} height={20} width={20} alt="image"/>
+      <div className="relative max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] grid place-items-center ">
+        {user?.image ? (
+          <Image className="  text-4xl text-white rounded-full" src={user?.image} height={90} width={90} alt="image" />
+        ) : (
+          user.name.charAt(0).toUpperCase()
+        )}
       </div>
 
       <div>
